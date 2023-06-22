6b91a83743c4e975630c87f7fa2b3469b3d45fb8
root <kijanamgeni3@gmail.com>
2023-06-22 12:38:26 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: - The first integer
+ * @b: - The second integer
+ * Return: Always 00
+ */
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}

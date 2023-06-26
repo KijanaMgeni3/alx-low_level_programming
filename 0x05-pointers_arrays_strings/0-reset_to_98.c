a42db372f429d4f7a0d14e61f30aa43ed71a4c92
root <kijanamgeni3@gmail.com>
2023-06-26 03:33:05 -0700

    0-98  0-main.c  0-reset_to_98.c  1-main.c  1-swap  1-swap.c  main.h  README.md

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function takes pointer to an int as a parameter
+ * @n: - input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap int - swaps the values of two integers
+ * @a: integers to swap
+ * @b: integers to swap
+ */
+void swap_int(int *a, int *b)
+{
+int m;
+m = *a;
+*a = *b;
+*b = m;
+}
